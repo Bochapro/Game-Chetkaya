a51d7b1f586c1af02a630f54d3ec70e827ea8ef4
LAPTOP-HLMGFUS7\Димас <9042865304@mail.ru>
2020-12-15 16:43:53 +0300

    Добавил анимацию прыжка, бега, статичности

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -10,6 +10,13 @@ public class Character : MonoBehaviour
     [SerializeField]
     private float jumpForce = 15.0F;
 
+    private bool isGrounded = false;
+
+    private CharState State
+    {
+        get { return (CharState)animator.GetInteger("State"); }
+        set { animator.SetInteger("State", (int)value); }
+    }
 
     new private Rigidbody2D rigidbody;
     private Animator animator;
@@ -23,10 +30,16 @@ public class Character : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        CheckGround();
+    }
+
     private void Update()
     {
+        if (isGrounded) State = CharState.Idle;
         if (Input.GetButton("Horizontal")) Run();
-        if (Input.GetButtonDown("Jump")) Jump();
+        if (isGrounded && Input.GetButtonDown("Jump")) Jump();
     }
 
     private void Run()
@@ -35,14 +48,30 @@ public class Character : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
-      
+        sprite.flipX = direction.x < 0.0F;
+        if (isGrounded) State = CharState.Run;
     }
 
     private void Jump()
     {
+        State = CharState.Jump;
         rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
     }
 
+    private void CheckGround()
+    {
+        if (!isGrounded) State = CharState.Jump;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.3F);
+        isGrounded = colliders.Length > 1;
+
+    }
     /*жопажопажопажопа
      */
+}
+
+public enum CharState
+{
+    Idle,
+    Run,
+    Jump
 }
