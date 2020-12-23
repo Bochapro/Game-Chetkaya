6d70fc2e7aea6c85ebbcb7c3cf36f46443827197
LAPTOP-HLMGFUS7\Димас <9042865304@mail.ru>
2020-12-23 03:10:41 +0300

    Научил монстра ходить и биться от стенок

--- Assets/Scripts/MoveableMonster.cs
+++ Assets/Scripts/MoveableMonster.cs
@@ -9,16 +9,37 @@ public class MoveableMonster : Monster
 
     private Bullet bullet;
 
+    private SpriteRenderer sprite;
+
+    private Vector3 direction; 
+
     protected override void Awake()
     {
+        sprite = GetComponentInChildren<SpriteRenderer>();
         bullet = Resources.Load<Bullet>("Bullet");
     }
 
+    protected override void Start()
+    {
+        direction = transform.right;
+    }
+    protected override void Update()
+    {
+        Move();
+    }
 
     protected override void OnTriggerEnter2D(Collider2D collider)
     {
         Bullet bullet = collider.GetComponent<Bullet>();
 
         if (bullet) ReceiveDamage();
+    }
+
+
+    private void Move()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.5F + transform.right * direction.x * 0.7F, 0.2F);
+        if (colliders.Length > 0) direction *= -1.0F;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
     }
 }
